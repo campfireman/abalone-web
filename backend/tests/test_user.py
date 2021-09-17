48c48e887b6fe271412d3933a333dbaca62b506d
Ture <ture.cl.business@gmail.com>
2021-09-17 15:33:47 +0200

    Add test for token creation

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -1,18 +1,46 @@
 from fastapi import status
 from fastapi.testclient import TestClient
+from mongoengine import connect, disconnect
+from src import models
+from src import operations as op
+from src import schemas, settings
 from src.api import app
 
 client = TestClient(app)
 
+disconnect()
+connect(
+    db=settings.TEST_DB_NAME,
+    host=settings.DB_HOST,
+    port=settings.DB_PORT,
+    username=settings.DB_USERNAME,
+    password=settings.DB_PASSWORD,
+    authentication_source='admin',
+)
+
+TEST_USER = schemas.NewUser(
+    username='peterpopper',
+    email='peter@popper.com',
+    password='password',
+)
+
 
 def test_created_user_valid():
-    test_user = {
-        'username': 'peterpopper',
-        'email': 'peter@popper.com',
-        'password': 'password',
-    }
-    response = client.post("/user", json=test_user)
+    response = client.post("/user", json=TEST_USER.dict())
     body = response.json()
     assert response.status_code == status.HTTP_201_CREATED
-    assert body['username'] == test_user['username']
-    assert body['email'] == test_user['email']
+    assert body['username'] == TEST_USER.username
+    assert body['email'] == TEST_USER.email
+    # delete user created during request
+    op.user_delete(TEST_USER.username)
+
+
+def test_user_login():
+    credentials = schemas.UserCredentials(
+        username=TEST_USER.username,
+        password=TEST_USER.password,
+    )
+    new_user = op.user_create(TEST_USER)
+    response = client.post("/user/login", json=credentials.dict())
+    assert response.status_code == status.HTTP_200_OK
+    new_user.delete()
