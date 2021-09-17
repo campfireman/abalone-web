25a99093b6be00570e636989f62678f5ca591170
Ture <ture.cl.business@gmail.com>
2021-09-17 15:26:33 +0200

    Cleanup database after testing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,5 +1,14 @@
+from pymongo import MongoClient
 from src import settings
 
 
 def pytest_unconfigure(config):
     print('Starting cleanup...')
+    print('Deleting database...')
+    mongo_client = MongoClient(
+        host=settings.DB_HOST,
+        port=settings.DB_PORT,
+        username=settings.DB_USERNAME,
+        password=settings.DB_PASSWORD,
+    )
+    mongo_client.drop_database(settings.TEST_DB_NAME)
